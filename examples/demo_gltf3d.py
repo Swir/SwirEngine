3ef2e4aa419076fc16fecbe295268e941b93c450
Jan Szczerbaty Uśmiechnięty <swirjestem@hotmail.com>
2026-09-11 20:54:48 +0200

    docs: add glTF 3D example

---
+++ examples/demo_gltf3d.py
@@ -0,0 +1,24 @@
+from swirengine import Game, Vec3
+
+
+game = Game("SwirEngine glTF Viewer", mode="3d", width=1280, height=720)
+model = game.gltf("models/model.gltf")
+model.position = Vec3(0.0, 0.0, -4.0)
+game.camera.look_at(model.position)
+game.show_debug()
+
+
+@game.update
+def move_camera(dt: float) -> None:
+    speed = 3.5 * dt
+    if game.key("W"):
+        game.camera.move_local(forward=speed)
+    if game.key("S"):
+        game.camera.move_local(forward=-speed)
+    if game.key("A"):
+        game.camera.move_local(right=-speed)
+    if game.key("D"):
+        game.camera.move_local(right=speed)
+
+
+game.run()
