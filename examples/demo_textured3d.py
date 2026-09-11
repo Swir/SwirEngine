59939bf70532b92016035337f22034345fcc1cd6
Jan Szczerbaty Uśmiechnięty <swirjestem@hotmail.com>
2026-09-11 19:53:16 +0200

    docs: add textured 3D example

---
+++ examples/demo_textured3d.py
@@ -0,0 +1,20 @@
+from swirengine import Game, Material3D, Vec3
+
+
+game = Game("SwirEngine Textured 3D", mode="3d")
+material = Material3D(
+    texture=game.assets.require("models/crate.png"),
+    ambient=0.3,
+    diffuse=0.7,
+)
+crate = game.obj("models/crate.obj", material=material)
+crate.position = Vec3(0.0, 0.0, -4.0)
+game.camera.look_at(crate.position)
+
+
+@game.update
+def spin(dt):
+    crate.rotation.y += 35.0 * dt
+
+
+game.run()
