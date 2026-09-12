9b75cc1a9593ad9f1265fe8f28dcc14be3b6a9b8
Jan Szczerbaty Uśmiechnięty <swirjestem@hotmail.com>
2026-09-12 17:50:52 +0200

    test: simplify transform gizmo coverage

--- tests/test_editor_gizmo.py
+++ tests/test_editor_gizmo.py
@@ -1,14 +1,15 @@
 import pytest
 
-from swirengine import Cube3D, EditorTransformGizmo, Scene, Sprite2D, Vec3
+from swirengine import Cube3D, EditorTransformGizmo, EditorWorkspace, Scene, Sprite2D, Vec3
 from swirengine.editor_workspace import EditorViewportState
 
 
 def test_gizmo_snapshot_exposes_3d_transform_capabilities():
     scene = Scene()
     cube = scene.add(Cube3D(position=Vec3(1.0, 2.0, 3.0), rotation=Vec3(4.0, 5.0, 6.0)))
-    gizmo = EditorTransformGizmo.from_workspace(__import__("swirengine").EditorWorkspace(scene))
-    gizmo.inspector.select(cube)
+    workspace = EditorWorkspace(scene)
+    workspace.select(cube)
+    gizmo = EditorTransformGizmo.from_workspace(workspace)
 
     snapshot = gizmo.snapshot()
 
@@ -22,8 +23,6 @@ def test_gizmo_snapshot_exposes_3d_transform_capabilities():
 def test_gizmo_3d_translation_uses_snapping_and_inspector_undo():
     scene = Scene()
     cube = scene.add(Cube3D(position=Vec3(0.2, 2.0, 3.0)))
-    from swirengine import EditorWorkspace
-
     workspace = EditorWorkspace(scene)
     workspace.select(cube)
     gizmo = EditorTransformGizmo.from_workspace(workspace)
@@ -42,8 +41,6 @@ def test_gizmo_3d_translation_uses_snapping_and_inspector_undo():
 def test_gizmo_applies_viewport_mode_and_snap_settings():
     scene = Scene()
     cube = scene.add(Cube3D(rotation=Vec3(0.0, 2.0, 0.0)))
-    from swirengine import EditorWorkspace
-
     workspace = EditorWorkspace(scene)
     workspace.select(cube)
     gizmo = EditorTransformGizmo.from_workspace(workspace)
@@ -57,8 +54,6 @@ def test_gizmo_applies_viewport_mode_and_snap_settings():
 def test_gizmo_supports_existing_2d_xy_and_scalar_rotation_convention():
     scene = Scene()
     sprite = scene.add(Sprite2D("hero.png", x=3.0, y=5.0, rotation=10.0))
-    from swirengine import EditorWorkspace
-
     workspace = EditorWorkspace(scene)
     workspace.select(sprite)
     gizmo = EditorTransformGizmo.from_workspace(workspace)
@@ -76,8 +71,6 @@ def test_gizmo_supports_existing_2d_xy_and_scalar_rotation_convention():
 def test_gizmo_uniformly_scales_cube_size_and_validates_axes():
     scene = Scene()
     cube = scene.add(Cube3D(size=2.0))
-    from swirengine import EditorWorkspace
-
     workspace = EditorWorkspace(scene)
     workspace.select(cube)
     gizmo = EditorTransformGizmo.from_workspace(workspace)
@@ -96,8 +89,6 @@ def test_gizmo_uniformly_scales_cube_size_and_validates_axes():
 def test_disabled_viewport_gizmo_rejects_mutation():
     scene = Scene()
     cube = scene.add(Cube3D())
-    from swirengine import EditorWorkspace
-
     workspace = EditorWorkspace(scene)
     workspace.select(cube)
     gizmo = EditorTransformGizmo.from_workspace(workspace)
