37a7ed45caf1481bec11ebba2938ea3b2e824c60
Wojciech <swirjestem@hotmail.com>
2026-09-14 12:49:19 +0200

    feat(network): export gameplay networking API

--- src/swirengine/__init__.py
+++ src/swirengine/__init__.py
@@ -116,6 +116,16 @@ from .input import (
 )
 from .live_development import LiveDevelopmentHub, LiveDevelopmentResult
 from .math.types import Color, Transform, Vec2, Vec3
+from .network_gameplay import (
+    ConnectionState,
+    GameplayMessage,
+    GameplaySession,
+    MessageKind,
+    MessageRouter,
+    NetworkDiagnostics,
+    RPCRegistry,
+    RPCResult,
+)
 from .networking import (
     NetworkAddress,
     NetworkPacket,
@@ -163,6 +173,7 @@ __all__ = [
     "Camera3D",
     "CollisionWorld2D",
     "Color",
+    "ConnectionState",
     "Cube3D",
     "CubemapFaces",
     "CubemapGPUTexture",
@@ -211,6 +222,8 @@ __all__ = [
     "GPUTextureInvalidation",
     "Game",
     "GamepadSnapshot",
+    "GameplayMessage",
+    "GameplaySession",
     "GizmoApplyResult",
     "GizmoTransformSnapshot",
     "GltfPrimitiveAsset",
@@ -232,7 +245,10 @@ __all__ = [
     "Material3D",
     "Mesh3D",
     "MeshData",
+    "MessageKind",
+    "MessageRouter",
     "NetworkAddress",
+    "NetworkDiagnostics",
     "NetworkPacket",
     "PackagingProfile",
     "PacketStreamDecoder",
@@ -254,6 +270,8 @@ __all__ = [
     "ProjectExporter",
     "PropertyEdit",
     "PygameAudioBackend",
+    "RPCRegistry",
+    "RPCResult",
     "Rectangle2D",
     "ReloadResult",
     "RendererAssetBridge",
